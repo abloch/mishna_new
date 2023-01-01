e605193c545a7af68a535c050fd5a1f3a28a1414
akiva <bloch.akiva@gmail.com>
2023-01-01 07:46:06 +0200

    fixes with unvariated

--- get_next.py
+++ get_next.py
@@ -4,4 +4,5 @@ masechet, chapter, mishna = deserialize()
 while True:
     masechet, chapter, mishna = get_next_mishna(masechet, chapter, mishna)
     print(masechet, chapter, mishna)
+    print(get_mishna(masechet, chapter, mishna).strip())
     input("press enter to get next")
