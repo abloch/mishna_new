77bb66dbd329f2d82129cfaa19cdefe4a659757f
Akiva <bloch.akiva@gmail.com>
2025-08-03 11:24:21 +0300

    no mishna at tisha veav

--- is_yomtov.py
+++ is_yomtov.py
@@ -12,13 +12,19 @@ reply = requests.get(url).json()
 
 config = json.load(open(argv[1]))
 
+
 def send_to_telegram(message, group):
     token = config["TELEGRAM_TOKEN"]
     bot = telepot.Bot(token)
     bot.sendMessage(group, message)
 
+
 relevant = [item for item in reply['items'] if item.get("date") == today.isoformat()]
 if relevant:
+    tbav = any(item.get("hebrew") == "תשעה באב" for item in relevant)
+    if tbav:
+        send_to_telegram("Today is Tisha B'Av", 215513269)
+        exit(1)
     yomtov = any(item.get("yomtov") for item in relevant)
     msg = {"today is": relevant, "yomtov": yomtov}
     send_to_telegram(pformat(msg), 215513269)
